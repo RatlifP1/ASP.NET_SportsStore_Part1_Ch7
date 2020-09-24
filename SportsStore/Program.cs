310de564b4406b4264b8621e25e1d44b5688b083
Paul J. Ratliff <64442014+RatlifP1@users.noreply.github.com>
2020-09-24 09:56:12 -0400

    added reference
    Included reference

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,3 +1,16 @@
+/* 
+Class:			ISTA 421 Developing Cloud Applications
+Student:		Paul J. Ratliff
+Instructor:		Christine Lee
+Date:			09/24/2020
+Description:	This is a completed turtorial from Pro ASP.NET Core:3 
+                Develop Cloud-Ready Web Applications Using MVC, Blazor, and Razor Pages. By: Adam Freeman 
+Revised:
+Revised by:
+Revision:
+
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
